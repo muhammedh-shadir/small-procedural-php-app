a533f2d41430b5012ecb86fe6c7d31bbd2e635c8
Muhammedh Shadir <shadir@fatbeehive.com>
2025-02-16 02:48:04 +0300

    formate date

--- app/helpers.php
+++ app/helpers.php
@@ -6,3 +6,7 @@ function formatDollarAmount(float $amount): string {
     $isNegative = $amount < 0;
     return ($isNegative ? '-' : '') . '$' . number_format(abs($amount), 2);
 }
+
+function formatDate(string $date): string {
+    return date('M j Y', strtotime($date));
+}
