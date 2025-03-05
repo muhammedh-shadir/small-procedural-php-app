66747c5aea8e08ba81d0a6a2db6d1818644000fb
Muhammedh Shadir <shadir@fatbeehive.com>
2025-03-05 17:17:40 +0300

    update getTransactionFiles

--- app/App.php
+++ app/App.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 function getTransactionFiles(string $dirName): array {
 
     $files = [];
-    foreach(scandir(FILES_PATH) as $file) {
+    foreach(scandir($dirName) as $file) {
         if (is_dir($file)) {
             continue;
         }
